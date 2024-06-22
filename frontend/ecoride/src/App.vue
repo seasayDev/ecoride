e633a3eca8049153f110ff9b8b2a5b712c2a23e1
Boulahsen, Yasser <boulahsen.yasser@courrier.uqam.ca>
2024-06-22 16:07:38 -0400

    adding login structure

--- frontend/ecoride/src/App.vue
+++ frontend/ecoride/src/App.vue
@@ -1,16 +1,19 @@
 <template>
+  <NavBar />
   <router-view></router-view>
-  <childRouter />
+  <!-- <childRouter /> -->
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import childRouter from "./components/childRouter.vue";
 import Shark from "./components/Shark.vue";
+import NavBar from "./components/NavBar.vue";
 
 export default defineComponent({
   components: {
     childRouter,
+    NavBar
   },
   setup() { },
 });
---
+++ frontend/ecoride/src/components/NavBar.vue
@@ -0,0 +1,47 @@
+<template>
+    <nav>
+        <ul>
+            <li>
+                <router-link to="/">Home</router-link>
+            </li>
+            <li>
+                <router-link to="/login">Login</router-link>
+            </li>
+        </ul>
+    </nav>
+</template>
+  
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'NavBar'
+});
+</script>
+  
+<style scoped>
+nav {
+    background-color: #333;
+    padding: 1rem;
+}
+
+ul {
+    list-style: none;
+    display: flex;
+    gap: 1rem;
+}
+
+li {
+    display: inline;
+}
+
+a {
+    color: white;
+    text-decoration: none;
+}
+
+a:hover {
+    text-decoration: underline;
+}
+</style>
+  
