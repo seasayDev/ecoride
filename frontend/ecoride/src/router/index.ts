baf603dc68086680c877b4c5e855a654ac45b613
Joy Serena <gg891961@ens.uqam.ca>
2024-07-08 18:53:19 -0400

    Ajout des modifications avant de basculer sur la branche serenabranche

--- frontend/ecoride/src/router/index.ts
+++ frontend/ecoride/src/router/index.ts
@@ -10,6 +10,8 @@ import admin from '@/components/admin/admin.vue'
 import Aide from '@/components/aide/Aide.vue'
 import Map from '@/components/map/Map.vue'
 
+
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
