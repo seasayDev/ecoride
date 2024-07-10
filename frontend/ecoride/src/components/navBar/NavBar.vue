ead69ad06ce9059511aa85d926e7f49801c411e4
Joy Serena <gg891961@ens.uqam.ca>
2024-07-10 07:04:49 -0400

    ajout des modifications dans Navbar.vue

--- frontend/ecoride/src/components/navBar/NavBar.vue
+++ frontend/ecoride/src/components/navBar/NavBar.vue
@@ -23,10 +23,7 @@
                 <router-link v-if="userStore.user" to="/aide">Aide</router-link>
             </li>
 
-             <li>
-                <router-link v-if="userStore.user" to="/promotions">Promotions</router-link>
-            </li>
-
+             
             <li>
                 <router-link v-if="userStore.user" to="/" @click.prevent="deconnexion">Deconnexion</router-link>
             </li>
