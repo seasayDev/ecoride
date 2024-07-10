c744839f4ff5c7f603146cc7b5b97a865486e16e
Joy Serena <gg891961@ens.uqam.ca>
2024-07-10 07:06:22 -0400

    ajout des modifications dans index.ts

--- frontend/ecoride/src/router/index.ts
+++ frontend/ecoride/src/router/index.ts
@@ -1,18 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import login from '@/components/login/Login.vue'
-import register from '@/components/register/Register.vue'
-import ResetPassword from '@/components/resetPassword/ResetPassword.vue'
-import Home from '@/components/home/Home.vue'
-import Support from '@/components/support/Support.vue'
-import Profil from '@/components/profil/Profil.vue'
-import EditProfil from '@/components/editProfil/EditProfil.vue'
-import admin from '@/components/admin/admin.vue'
-import Aide from '@/components/aide/Aide.vue'
-import Map from '@/components/map/Map.vue'
-import Payement from '@/components/payement/Payement.vue'
-import ReservationDetails from '@/components/reservationDetails/ReservationDetails.vue'
-import Promotions from '@/components/promotions/promotion.vue'
-
+import { createRouter, createWebHistory } from 'vue-router';
+import login from '@/components/login/Login.vue';
+import register from '@/components/register/Register.vue';
+import ResetPassword from '@/components/resetPassword/ResetPassword.vue';
+import Home from '@/components/home/Home.vue';
+import Support from '@/components/support/Support.vue';
+import Profil from '@/components/profil/Profil.vue';
+import EditProfil from '@/components/editProfil/EditProfil.vue';
+import admin from '@/components/admin/admin.vue';
+import Aide from '@/components/aide/Aide.vue';
+import Map from '@/components/map/Map.vue';
+import Payement from '@/components/payement/Payement.vue';
+import ReservationDetails from '@/components/reservationDetails/ReservationDetails.vue';
+import Promotions from '@/components/promotions/promotion.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -31,6 +30,6 @@ const router = createRouter({
     { path: '/promotions', name: 'Promotions', component: Promotions, meta: { requiresAuth: true } },
     { path: '/reservation-details', name: 'ReservationDetails', component: ReservationDetails }
   ]
-})
+});
 
-export default router
+export default router;
