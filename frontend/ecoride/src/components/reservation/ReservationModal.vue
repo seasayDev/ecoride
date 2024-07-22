911fa84530dd3574319ada9f73e11d26ae18bb50
Moné Youaga, Jordan Stéve <mone_youaga.jordan_steve@courrier.uqam.com>
2024-07-22 02:29:36 -0400

    adjust

--- frontend/ecoride/src/components/reservation/ReservationModal.vue
+++ frontend/ecoride/src/components/reservation/ReservationModal.vue
@@ -1,5 +1,4 @@
 <template>
-  <!-- Modal content remains the same -->
   <div class="modal" id="reservationModal" tabindex="-1" role="dialog" aria-labelledby="reservationModalLabel"
     aria-hidden="true" v-if="isVisible">
     <div class="modal-dialog" role="document">
@@ -76,6 +75,7 @@ import { Trotinette } from '../scooterCard';
 import { userStore, getUserFromStorage } from "@/components/helpers/userSession";
 import { Location, Newscooter, GetTrotinettes } from '../admin';
 
+
 export default defineComponent({
   props: {
     isVisible: {
@@ -128,30 +128,20 @@ export default defineComponent({
       currentStep.value = 'form';
     };
 
-    const confirmReservation = async () => {
+    const confirmReservation = () => {
       const reservationDetails = {
-        trotinette_id: props.trotinette.id_trotinette,
+        trotinette: props.trotinette,
         user_id: userStore.user?.session.id_user,
-        start_date: reservationDate.value + ' ' + state.reservationTime,
-        end_date: reservationDate.value + ' ' + state.reservationTime, // Assuming the end time is the same as start time plus duration
-        pick_up_address: props.trotinette.location.id,
-        drop_off_address: state.dropOutLocation.id_location,
+        reservationDate: reservationDate.value,
+        reservationTime: state.reservationTime,
+        reservationDuration: reservationDuration.value,
+        dropOutLocation: state.dropOutLocation.id_location,
+        dropOutName: state.dropOutLocation.name,
+        totalCost: costToshow.value,
       };
 
-      try {
-        await fetch('http://localhost:5000/reserveTrotinette', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(reservationDetails),
-        });
-
-        localStorage.setItem('reservationDetails', JSON.stringify(reservationDetails));
-        router.push({ name: 'ReservationHistory' });
-      } catch (error) {
-        console.error('Error confirming reservation:', error);
-      }
+      localStorage.setItem('reservationDetails', JSON.stringify(reservationDetails));
+      router.push({ name: 'ReservationDetails' });
     };
 
     const today = computed(() => {
